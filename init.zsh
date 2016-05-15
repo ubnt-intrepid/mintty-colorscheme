8caf9012ed2649e46580c1389af98733b49ec6d8
Yusuke Sasaki <y_sasaki@nuem.nagoya-u.ac.jp>
2016-05-15 23:47:52 +0900

    minttyrc-style rcs

--- init.zsh
+++ init.zsh
@@ -2,10 +2,40 @@
 
 script_root="$(realpath $(dirname `realpath $0`))"
 
+__color()       { echo -ne '\eP\e]'$1';'$2'\a' }
+__colorscheme() { echo -ne '\eP\e]4;'$1';'$2'\a' }
+
+ForegroundColour() { __color 10 $2 }
+BackgroundColour() { __color 11 $2 }
+CursorColour()     { __color 12 $2 }
+
+Black()       { __colorscheme 0 $2 }
+Red()         { __colorscheme 1 $2 }
+Green()       { __colorscheme 2 $2 }
+Yellow()      { __colorscheme 3 $2 }
+Blue()        { __colorscheme 4 $2 }
+Magenta()     { __colorscheme 5 $2 }
+Cyan()        { __colorscheme 6 $2 }
+White()       { __colorscheme 7 $2 }
+BoldBlack()   { __colorscheme 8 $2 }
+BoldRed()     { __colorscheme 9 $2 }
+BoldGreen()   { __colorscheme 10 $2 }
+BoldYellow()  { __colorscheme 11 $2 }
+BoldBlue()    { __colorscheme 12 $2 }
+BoldMagenta() { __colorscheme 13 $2 }
+BoldCyan()    { __colorscheme 14 $2 }
+BoldWhite()   { __colorscheme 15 $2 }
+
+function __eval {
+  eval "$@"
+}
+
 colorscheme_main() {
 
-  if [[ "$#" -ne 1 ]]; then
-    echo "Usage: colorscheme [--list] name"
+  if (( $# != 1 )); then
+    echo "Usage:"
+    echo "  colorscheme [--list]"
+    echo "  colorscheme name"
     return 1
   fi
 
@@ -15,12 +45,6 @@ colorscheme_main() {
     done
     return 0
   fi
-
-  function __eval {
-    __color()       { echo -ne '\eP\e]'$1';#'$2'\a' }
-    __colorscheme() { echo -ne '\eP\e]4;'$1';#'$2'\a' }
-    eval "$@"
-  }
 
   # read schemes
   lines=( ${(@f)"$(< $script_root/schemes/$1)"} )
