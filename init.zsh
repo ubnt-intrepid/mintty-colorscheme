2579eb1edad942858ba46f5b10ad9ba821b4d18d
Yusuke Sasaki <y_sasaki@nuem.nagoya-u.ac.jp>
2016-05-17 03:04:52 +0900

    update

--- init.zsh
+++ init.zsh
@@ -5,28 +5,30 @@ script_root="$(realpath $(dirname `realpath $0`))"
 __color()       { echo -ne '\eP\e]'$1';'$2'\a' }
 __colorscheme() { echo -ne '\eP\e]4;'$1';'$2'\a' }
 
-ForegroundColour() { __color 10 $2 }
-BackgroundColour() { __color 11 $2 }
-CursorColour()     { __color 12 $2 }
+ForegroundColour() { __color 10 "$@" }
+BackgroundColour() { __color 11 "$@" }
+CursorColour()     { __color 12 "$@" }
 
-Black()       { __colorscheme 0 $2 }
-Red()         { __colorscheme 1 $2 }
-Green()       { __colorscheme 2 $2 }
-Yellow()      { __colorscheme 3 $2 }
-Blue()        { __colorscheme 4 $2 }
-Magenta()     { __colorscheme 5 $2 }
-Cyan()        { __colorscheme 6 $2 }
-White()       { __colorscheme 7 $2 }
-BoldBlack()   { __colorscheme 8 $2 }
-BoldRed()     { __colorscheme 9 $2 }
-BoldGreen()   { __colorscheme 10 $2 }
-BoldYellow()  { __colorscheme 11 $2 }
-BoldBlue()    { __colorscheme 12 $2 }
-BoldMagenta() { __colorscheme 13 $2 }
-BoldCyan()    { __colorscheme 14 $2 }
-BoldWhite()   { __colorscheme 15 $2 }
+Black()       { __colorscheme 0 "$@" }
+Red()         { __colorscheme 1 "$@" }
+Green()       { __colorscheme 2 "$@" }
+Yellow()      { __colorscheme 3 "$@" }
+Blue()        { __colorscheme 4 "$@" }
+Magenta()     { __colorscheme 5 "$@" }
+Cyan()        { __colorscheme 6 "$@" }
+White()       { __colorscheme 7 "$@" }
+BoldBlack()   { __colorscheme 8 "$@" }
+BoldRed()     { __colorscheme 9 "$@" }
+BoldGreen()   { __colorscheme 10 "$@" }
+BoldYellow()  { __colorscheme 11 "$@" }
+BoldBlue()    { __colorscheme 12 "$@" }
+BoldMagenta() { __colorscheme 13 "$@" }
+BoldCyan()    { __colorscheme 14 "$@" }
+BoldWhite()   { __colorscheme 15 "$@" }
 
-__eval() { eval "$@" }
+__eval() {
+  eval "${@%%=*} ${@##*=}"
+}
 
 colorscheme_main() {
 
